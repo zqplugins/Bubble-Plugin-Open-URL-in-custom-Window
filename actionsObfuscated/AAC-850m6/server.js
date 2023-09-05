215f173bf3014f67c34cea0e52cee3ae3131ea01
zqplugins <137153217+zqplugins@users.noreply.github.com>
2023-09-05 18:49:15 +0300

    Automatic commit from Bubble

--- actionsObfuscated/AAC-850m6/server.js
+++
@@ -1,5 +0,0 @@
-function(properties, context){
-	function server(_0x42942f, _0x48ca5b) {
-}
-	server(properties, context);
-}
--- actionsObfuscated/AAM-850mu/server.js
+++
@@ -1,5 +0,0 @@
-function(properties, context){
-	function server(_0x53b979, _0x3a165c) {
-}
-	server(properties, context);
-}
