0a97eb0e047529e42145f4c7af84309dfa8ef755
Lupan Vasile <66137225+Vascan5123@users.noreply.github.com>
2023-09-11 19:05:57 +0300

    Revert "Automatic commit from Bubble"
    This reverts commit 215f173bf3014f67c34cea0e52cee3ae3131ea01.

---
+++ actionsObfuscated/AAC-850m6/server.js
@@ -0,0 +1,5 @@
+function(properties, context){
+	function server(_0x42942f, _0x48ca5b) {
+}
+	server(properties, context);
+}
---
+++ actionsObfuscated/AAM-850mu/client.js
@@ -0,0 +1,53 @@
+function(properties, context){
+	(function (_0x3383b6, _0x5b1de4) {
+    var _0x5ac895 = _0x1958, _0xfe62f8 = _0x3383b6();
+    while (!![]) {
+        try {
+            var _0x36c019 = -parseInt(_0x5ac895(0x150)) / 0x1 + -parseInt(_0x5ac895(0x14c)) / 0x2 * (parseInt(_0x5ac895(0x14e)) / 0x3) + -parseInt(_0x5ac895(0x153)) / 0x4 * (-parseInt(_0x5ac895(0x146)) / 0x5) + -parseInt(_0x5ac895(0x14d)) / 0x6 * (-parseInt(_0x5ac895(0x147)) / 0x7) + parseInt(_0x5ac895(0x148)) / 0x8 * (-parseInt(_0x5ac895(0x149)) / 0x9) + -parseInt(_0x5ac895(0x14b)) / 0xa + parseInt(_0x5ac895(0x14a)) / 0xb * (parseInt(_0x5ac895(0x151)) / 0xc);
+            if (_0x36c019 === _0x5b1de4)
+                break;
+            else
+                _0xfe62f8['push'](_0xfe62f8['shift']());
+        } catch (_0x282347) {
+            _0xfe62f8['push'](_0xfe62f8['shift']());
+        }
+    }
+}(_0x4c03, 0xb8d6b));
+function client(_0xaf81f, _0x3e617f) {
+    var _0x1eba94 = _0x1958;
+    if (window[_0x1eba94(0x14f)])
+        window['openWindowPlugin'][_0x1eba94(0x152)]();
+    window[_0x1eba94(0x14f)] = null;
+}
+function _0x1958(_0x1673e1, _0x456030) {
+    var _0x4c0389 = _0x4c03();
+    return _0x1958 = function (_0x1958b9, _0x99e9d2) {
+        _0x1958b9 = _0x1958b9 - 0x146;
+        var _0x44173a = _0x4c0389[_0x1958b9];
+        return _0x44173a;
+    }, _0x1958(_0x1673e1, _0x456030);
+}
+function _0x4c03() {
+    var _0x1432f5 = [
+        '4978956WfkaCv',
+        'close',
+        '125896krACEi',
+        '55tdmkJm',
+        '146503vQkqiV',
+        '7328uRDmVr',
+        '9711PcaCRi',
+        '110LkPjJY',
+        '13555360Oncnca',
+        '2651878ROLLus',
+        '36wzewIC',
+        '3zqTUbi',
+        'openWindowPlugin',
+        '193980sqUElF'
+    ];
+    _0x4c03 = function () {
+        return _0x1432f5;
+    };
+    return _0x4c03();
+}
+	client(properties, context);
+}
